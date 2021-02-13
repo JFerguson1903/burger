63a5db9bc6a5797ae02ef68bf0c876d7d9a0c8fd
Jessica Ferguson <jferguson1903@gmail.com>
2021-02-12 22:09:15 -0600

    created seed content

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
-write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Classic Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Farmer', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
